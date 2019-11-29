2738e7d483624096e2d890f62cc1b26ebec3a460
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2019-11-29 03:50:24 +0200

    Add project files.

---
+++ NullableRepro/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using ReactiveUI;
+
+
+
+namespace WpfApp3
+{
+  public class Vm : IRoutableViewModel
+  {
+    public string UrlPathSegment { get; }
+    public IScreen HostScreen { get; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangingEventHandler PropertyChanging;
+
+    public void RaisePropertyChanged(PropertyChangedEventArgs args) => throw new NotImplementedException();
+    public void RaisePropertyChanging(PropertyChangingEventArgs args) => throw new NotImplementedException();
+  }
+
+
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : ReactiveWindow<Vm>
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
